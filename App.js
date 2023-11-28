08dbfbc59c2851c96c682f0b1004c1d08a112517
EnzoFelyx <enzofelyx@gmail.com>
2023-11-27 21:19:17 -0300

    fixed

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
-import { StatusBar, SafeAreaView, View, Dimensions, StyleSheet } from 'react-native';
-import React, { useEffect } from 'react'
-import { useFonts, Quicksand_400Regular, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
-import api from './src/servicos/api'
-
-
+import { Quicksand_400Regular, Quicksand_700Bold, useFonts } from '@expo-google-fonts/quicksand';
+import React from 'react';
+import { Dimensions, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import RotasLogin from './src/rotas/Rotas_login';
-import Rotas from './src/rotas/Rotas';
-
-
-
 const window = Dimensions.get('window');
 
-
 export default function App() {
-
-  useEffect(() => {
-    async function pegarDardos() {
-      const resultado = await api.get('/users')
-
-    }
-    pegarDardos()
-  }, [])
-
-
-
 
   const [fontCarregada] = useFonts({
     "QuicksandRegular": Quicksand_400Regular,
